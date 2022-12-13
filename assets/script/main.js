dc915390e1c79f1f42ecac5cfc24eaf93ac05c3c
ThomasScheepers <thomas@scheepers.com>
2022-12-13 09:47:48 +0100

    5 vd 10

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,65 @@
-let interaction = document.querySelector('a:nth-of-type(12)')
+// addEventListener( CLICK ) function
 
-interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
+let clickInteraction = document.querySelector('a:nth-of-type(1)')
 
-function jumpHandler() {
-  interaction.classList.toggle('jump')
+clickInteraction.addEventListener('click', errorHandler)
+clickInteraction.addEventListener('animationend', errorHandler)
+
+function errorHandler() {
+  clickInteraction.classList.toggle('error')
+}
+
+// addEventListener( MOUSEOVER ) function
+
+let hoverInteraction = document.querySelector('a:nth-of-type(2)')
+
+hoverInteraction.addEventListener('mouseover', spinHandler)
+hoverInteraction.addEventListener('animationend', spinHandler)
+
+function spinHandler() {
+  hoverInteraction.classList.toggle('spin')
+}
+
+// addEventListener( DOUBLECLICK ) function
+
+let doubleInteraction = document.querySelector('a:nth-of-type(3)')
+
+doubleInteraction.addEventListener('dblclick', doubleHandler)
+doubleInteraction.addEventListener('animationend', doubleHandler)
+
+function doubleHandler() {
+  doubleInteraction.classList.toggle('bounce')
+}
+
+// addEventListener( WHEEL ) function
+
+let wheelInteraction = document.querySelector('a:nth-of-type(4)')
+
+wheelInteraction.addEventListener('wheel', wheelHandler)
+wheelInteraction.addEventListener('animationend', wheelHandler)
+
+function wheelHandler() {
+  wheelInteraction.classList.toggle('discoDance')
+}
+
+// addEventListener( MOUSEOUT ) function
+
+let mouseoutInteraction = document.querySelector('a:nth-of-type(5)')
+
+mouseoutInteraction.addEventListener('mouseout', exitHandler)
+mouseoutInteraction.addEventListener('animationend', exitHandler)
+
+function exitHandler() {
+  mouseoutInteraction.classList.toggle('nietWegGaan')
+}
+
+// addEventListener( MOUSEDOWN ) function
+
+let mouseupInteraction = document.querySelector('a:nth-of-type(6)')
+
+mouseupInteraction.addEventListener('resize', exitHandler)
+mouseupInteraction.addEventListener('animationend', exitHandler)
+
+function exitHandler() {
+  mouseupInteraction.classList.toggle('small')
 }
